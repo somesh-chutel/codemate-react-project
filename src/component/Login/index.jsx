26437f2c7859446de95a7f187e02439e0ec93076
srocknik <somesh.chutel90@gmail.com>
2024-04-06 22:19:39 +0530

    seventh commit

--- src/component/Login/index.jsx
+++ src/component/Login/index.jsx
@@ -1,11 +1,69 @@
+import { useState } from "react";
 import "./index.css";
 
 const Login = () => {
+
+  /*
+      For single variable we can use below mentioned syntax of useState
+
+      const [username,setUsername] = useState("someshchutel");
+  */
+
+  //For multiple values we can use below mentioned syntax of useState
+  const [allValues,setValues] = useState({
+      username:"",
+      password:"",
+      showErrorMsg:false,
+      errorMsg:""
+  });
+
+    const onFetchUserData = async(event)=>{
+        event.preventDefault();
+
+
+        const url = "http://localhost:3001/login";
+
+        const userDetails = {
+          username: allValues.username,
+          password: allValues.password
+        }
+
+        const options = {
+          method:"POST",
+          headers:{
+            "Content-Type":"application/json"
+          },
+          body : JSON.stringify(userDetails)
+        }
+
+        const response = await fetch(url,options);
+        const fetchData = await response.json();
+        console.log(fetchData)
+        if(response.ok===true){
+          setValues({...allValues,showErrorMsg:false});
+        }
+        else{
+          setValues({...allValues,showErrorMsg:true,errorMsg:fetchData.errorMsg});
+        }
+
+    }
+
+    const onChangeUserName = (event)=>{
+        setValues({...allValues,username:event.target.value})
+
+    }
+
+    const onChangeUserPassword = (event)=>{
+      setValues({...allValues,password:event.target.value})
+    }
+
+
+
   return (
     <div className="login-cont container-fluid">
       {/* Bootstrap form starts from here  */}
 
-      <form className=" text-white bg-dark p-3 rounded form-cont">
+      <form className=" text-white bg-dark p-3 rounded form-cont" onSubmit={onFetchUserData}>
         <div className="text-center">
             <img className="web-logo" src="https://assets.ccbp.in/frontend/react-js/logo-img.png" alt="" />
         </div>
@@ -14,10 +72,11 @@ const Login = () => {
             Email address
           </label>
           <input
-            type="email"
+            type="text"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            onChange={onChangeUserName}
           />
           <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
@@ -31,11 +90,14 @@ const Login = () => {
             type="password"
             className="form-control"
             id="exampleInputPassword1"
+            onChange={onChangeUserPassword}
           />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
+        {/*Turnery Opertor*/}
+        {allValues.showErrorMsg?(<p className="err-msg">{`* ${allValues.errorMsg}`}</p>):null}
       </form>
 
       {/* Bootstrap form ends from here  */}
